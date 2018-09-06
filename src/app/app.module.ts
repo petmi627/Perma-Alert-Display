13205c034b3cb9f147c147534c87060dd58689d4
Mike Peters <mike@petmi627.lu>
2018-09-06 14:14:40 +0200

    Generated Ambulance Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ClockComponent } from './components/display/clock/clock.component';
+import { AmbulanceComponent } from './components/display/ambulance/ambulance.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClockComponent
+    ClockComponent,
+    AmbulanceComponent
   ],
   imports: [
     BrowserModule
