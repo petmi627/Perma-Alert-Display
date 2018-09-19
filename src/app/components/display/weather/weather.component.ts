d95ff2741cafd21ecc5c77e9317fa5eca1919287
Mike Peters <mike@petmi627.lu>
2018-09-19 15:07:33 +0200

    Added Weather Service to Weather Component

--- src/app/components/display/weather/weather.component.ts
+++ src/app/components/display/weather/weather.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {WeatherService} from '../../../services/weather/weather.service';
+import {Weather} from '../../../models/weather';
 
 @Component({
   selector: 'app-weather',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
+  weatherForecast: Weather[];
+  currentWeather: Weather;
 
-  constructor() { }
+  constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
+    this.weatherForecast = this.weatherService.getWeatherForecast();
+    this.currentWeather = this.weatherService.getCurrentWeather();
   }
 
 }
