717bc79140a1203a12091f5335d9297f1ab631fe
Mike Peters <mike@petmi627.lu>
2018-09-25 09:33:35 +0200

    Updated Layout of Weather Component

--- src/app/components/display/weather/weather.component.ts
+++ src/app/components/display/weather/weather.component.ts
@@ -10,12 +10,17 @@ import {Weather} from '../../../models/weather';
 export class WeatherComponent implements OnInit {
   weatherForecast: Weather[];
   currentWeather: Weather;
+  weatherAlertImage: string = 'http://alarm.meteozentral.lu/images/map/letzebuerg_index.png';
 
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
     this.weatherForecast = this.weatherService.getWeatherForecast();
     this.currentWeather = this.weatherService.getCurrentWeather();
+
+    setInterval(() => {
+          this.weatherAlertImage = 'http://alarm.meteozentral.lu/images/map/letzebuerg_index.png';
+    }, 600000);
   }
 
 }
