18c4bbbcae7c56faceec93e225c545d253a298e3
Mike Peters <mike@petmi627.lu>
2018-11-10 13:30:07 +0100

    Added API Url environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  api_urls: {
+    headline: 'http://localhost:5000/display/headlines',
+  }
 };
 
 /*
