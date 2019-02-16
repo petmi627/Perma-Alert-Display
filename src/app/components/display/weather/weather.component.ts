b949c1e79e1b9c63e8a6d2abd2c4ad2c061060ab
Mike Peters <mike@petmi627.lu>
2019-02-16 20:44:26 +0100

    Updated Weather Component

--- src/app/components/display/weather/weather.component.ts
+++ src/app/components/display/weather/weather.component.ts
@@ -18,24 +18,32 @@ export class WeatherComponent implements OnInit {
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
-    this.weatherService.getCurrentWeather().subscribe(weather => {
-          this.currentWeather = weather;
-          this.loadedCurrentWeather = true;
-    });
-      this.weatherService.getWeatherForecast().subscribe(forecast => {
-          let forecast_list = [];
-          forecast.forEach((weather, i) => {
-            if (i < 3) {
-             forecast_list.push(weather);
-            }
-          });
-          this.weatherForecast = forecast_list;
-          this.loadedForecast = true;
-      });
+    this.getWeather();
+
+      setInterval(() => {
+          this.getWeather();
+      }, 60 * 60 * 3600);
 
     setInterval(() => {
           this.weatherAlertImage = 'http://alarm.meteozentral.lu/images/map/letzebuerg_index.png';
     }, 600000);
+  }
+
+  getWeather() {
+      this.weatherService.getCurrentWeather().subscribe(weather => {
+          this.currentWeather = weather;
+          this.loadedCurrentWeather = true;
+      });
+      this.weatherService.getWeatherForecast().subscribe(forecast => {
+          let forecast_list = [];
+          forecast.forEach((weather, i) => {
+              if (i < 3) {
+                  forecast_list.push(weather);
+              }
+          });
+          this.weatherForecast = forecast_list;
+          this.loadedForecast = true;
+      });
   }
 
 }
