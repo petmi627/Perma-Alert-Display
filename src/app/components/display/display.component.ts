8a14b75ce2b15d1c9d79156504ad6293a173b6c2
Mike Peters <mike@petmi627.lu>
2019-02-19 21:38:55 +0100

    Integrated Cis Service in Display Component

--- src/app/components/display/display.component.ts
+++ src/app/components/display/display.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import {CisService} from '../../services/cis/cis.service';
+import {Cis} from '../../models/cis';
 
 @Component({
   selector: 'app-display',
@@ -8,14 +10,20 @@ import { Location } from '@angular/common';
   styleUrls: ['./display.component.css']
 })
 export class DisplayComponent implements OnInit {
-  vehicle = ['AMB1-Diekirch', 'AMB2-Diekirch', 'INCSA'];
+  cis;
+  loaded: boolean = false;
 
   constructor(private route: ActivatedRoute,
+              private cisService: CisService,
               private location: Location) { }
 
   ngOnInit() {
-    const cis = this.route.snapshot.paramMap.get('cis');
-    console.log(cis);
+    const cis_location = this.route.snapshot.paramMap.get('cis');
+    this.cisService.getCis(cis_location).subscribe(cis => {
+        console.log(cis);
+        this.cis = cis;
+        this.loaded = true;
+    });
   }
 
 }
