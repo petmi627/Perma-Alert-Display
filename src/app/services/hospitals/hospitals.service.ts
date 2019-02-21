a56a2a297bb22433f5f6396bcdf2b6cf266d06ea
Mike Peters <mike@petmi627.lu>
2019-02-21 19:07:45 +0100

    Adapt Hospital Service to routing

--- src/app/services/hospitals/hospitals.service.ts
+++ src/app/services/hospitals/hospitals.service.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@angular/core';
 import {Hospitals} from '../../models/hospitals';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HospitalsService {
-  hospitalUrl = 'http://localhost:5000/display/hospitals';
-
   constructor(private http: HttpClient) {}
 
   getHospitals(): Observable<Hospitals[]> {
-    return this.http.get<Hospitals[]>(this.hospitalUrl);
+    let url = environment.api_urls.base_url + 'hospitals';
+
+    return this.http.get<Hospitals[]>(url);
   }
 }
