3bb9b3c231bfe33e2e7aa5d9fee506fb73c8edb5
Mike Peters <mike@petmi627.lu>
2019-03-02 21:34:51 +0100

    Updated Login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {LoginService} from '../../services/login/login.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {UserService} from '../../services/user/user.service';
 
 @Component({
@@ -15,7 +15,8 @@ export class LoginComponent implements OnInit {
 
     constructor(private loginService: LoginService,
                 private userService: UserService,
-                private router: Router) { }
+                private router: Router,
+                private route: ActivatedRoute) { }
 
     ngOnInit() {
         if (this.loginService.isLoggedIn()) {
@@ -25,6 +26,16 @@ export class LoginComponent implements OnInit {
                 this.router.navigate(['/']);
             });
         }
+
+        // TODO: Security Issue need to do a work around for Auto Login
+        this.route.queryParams.subscribe(params => {
+            if (params['username'] && params['password']) {
+                console.log(params);
+                this.username = params['username'];
+                this.password = params['password'];
+                this.onSubmit();
+            }
+        });
     }
 
     onSubmit() {
